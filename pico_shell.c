e27dd3e5a4a210690b81a9bded98165ce54d75a5
maselgazwy10-gif <maselgazwy10@gmail.com>
2026-07-25 12:47:13 +0300

    Update pico_shell.c

--- pico_shell.c
+++ pico_shell.c
@@ -1,29 +1,33 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-    char input[1024];
+    char input[65536];
+    int exit_status = 0; 
     
     while (1) {
         printf("Picoshell waiting for your command> ");
         
-        if (fgets(input, 1024, stdin) == NULL) {
+        if (fgets(input, sizeof(input), stdin) == NULL) {
             break; 
         }
         
         input[strcspn(input, "\n")] = 0;
         
-        char *my_argv[100]; 
+        char *my_argv[2048]; 
         int my_argc = 0;
         
         char *token = strtok(input, " ");
         
-        while (token != NULL) {
+        while (token != NULL && my_argc < 2047) {
             my_argv[my_argc] = token;
             my_argc++;
             token = strtok(NULL, " ");
         }
+        my_argv[my_argc] = NULL; 
         
         if (my_argc == 0) {
             continue;
@@ -38,6 +42,7 @@ int main() {
                 }
             }
             printf("\n");
+            exit_status = 0;
         }
         
         else if (strcmp(my_argv[0], "exit") == 0) {
@@ -46,28 +51,49 @@ int main() {
         }
         
         else if (strcmp(my_argv[0], "pwd") == 0) {
-            char my_array[4096];    
-            char *x = getcwd(my_array, 4096);
+            char my_array[65536]; 
+            char *x = getcwd(my_array, sizeof(my_array));
             if (x == NULL) {
                 printf("Error pwd failed\n");
+                exit_status = 1;
             } else {
                 printf("%s\n", my_array);
+                exit_status = 0;
             }
         }
         
         else if (strcmp(my_argv[0], "cd") == 0) {
             if (my_argc < 2) {
-                printf("error invalid number of arguments\n");
+                printf("cd: missing argument\n");
+                exit_status = 1;
             } 
             else if (chdir(my_argv[1]) != 0) {
-                printf("error\n");
+                printf("cd: %s: No such file or directory\n", my_argv[1]);
+                exit_status = 1;
+            } else {
+                exit_status = 0;
             }
         }
         
         else {
-            printf("Invalid command\n");
+            pid_t pid = fork();
+            if (pid < 0) {
+                perror("Fork failed");
+                exit_status = 1;
+            } else if (pid == 0) {
+                execvp(my_argv[0], my_argv);
+                
+                printf("%s: command not found\n", my_argv[0]);
+                exit(127); 
+            } else {
+                int status;
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status)) {
+                    exit_status = WEXITSTATUS(status);
+                }
+            }
         }
     }
     
-    return 0;
+    return exit_status;
 }
